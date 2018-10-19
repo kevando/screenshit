4c27d66e69a40f96026c04fad662ece38b81746d
kevando <khabich@gmail.com>
2018-10-19 01:28:48 -0400

    works decently well. app icon looks legit, etc

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# screen-shit
-Desktop application for managing screenshots
+### Screen Shit
+
+![demo](app/icon/96x96.png)
+
+
+Screen Shit is an open source application for your desktop that aids you after taking a screenshot. 
+
+
+
+#### Available features
+
+* Copy screen shot to clipboard
+
+
+
+
+## Roadmap
+* option to delete screenshot
+* option to archive screenshot 
+
+
+## Development
+
+I used electron for this
+
+
+```
+git clone [...]
+rm -rf node_modules
+npm install
+electron-forge start  
+
+
+```
+
+
+
+### Chokidar
+https://ourcodeworld.com/articles/read/160/watch-files-and-directories-with-electron-framework
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 'use strict';
 
-const {app, Menu, Tray, BrowserWindow, Notification} = require('electron');
+const {
+  app,
+  Menu,
+  Tray,
+  BrowserWindow,
+  Notification,
+  dialog,
+  clipboard,
+  nativeImage
+} = require('electron');
 
 var chokidar = require('chokidar');
 var fs = require('fs');
+var path = require('path');
+
+// const iconPath = path.join(___dirname, '')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -11,15 +23,8 @@ let mainWindow = null;
 let tray = null;
 let watcher = null;
 
+let desktopPath = null;
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function() {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform != 'darwin') {
-    app.quit();
-  }
-});
 
 
 // app.dock.hide();
@@ -27,16 +32,68 @@ app.on('window-all-closed', function() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 app.on('ready', () => {
-  createTray();
+  // createTray();
   createBrowserWindow();
 
   var screenshotPath = app.getPath('desktop');
   console.log('screenshotPath',screenshotPath)
-
+  //
   startWatcher(screenshotPath);
 
 
 });
+
+
+// -----------------------------------------------------
+// -----------------------------------------------------
+
+function createBrowserWindow() {
+
+  mainWindow = new BrowserWindow({
+    width: 900,
+    height: 500,
+    // skipTaskbar: true,
+    // autoHideMenuBar: true,
+    // backgroundColor: '#66CD00',
+    // show: false
+    // icon: path.join(__dirname, 'assets/icons/png/64x64.png')
+    // icon: __dirname + '/assets/icons/png/64x64.png'
+  });
+
+  // desktopPath = app.getPath('desktop')
+  //
+  // console.log('desktopPath',desktopPath);
+
+
+  // and load the index.html of the app.
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+  // now send the html some data
+  mainWindow.webContents.send('desktop-path', desktopPath);
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools();
+
+
+  mainWindow.on('minimize',function(event){
+    event.preventDefault();
+    mainWindow.hide();
+  });
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function() {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+
+
+
+}
+
+// -----------------------------------------------------
+// -----------------------------------------------------
 
 function createTray() {
 
@@ -67,126 +124,53 @@ function createTray() {
 
 }
 
-
-function createBrowserWindow() {
-
-
-  console.log('CREATE')
-  var notif = new Notification('New Screenshot!', {
-    'body':  'native html5 notif',
-    'icon': __dirname + '/assets/notificationIcon.png'
-  });
-
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 900,
-    height: 500,
-    // skipTaskbar: true,
-    // autoHideMenuBar: true,
-    // backgroundColor: '#66CD00',
-    // show: false
-    // icon: path.join(__dirname, 'assets/icons/png/64x64.png')
-    icon: __dirname + '/assets/icons/png/64x64.png'
-  });
-
-  // and load the index.html of the app.
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-
-
-  mainWindow.on('minimize',function(event){
-    event.preventDefault();
-    mainWindow.hide();
-  });
-
-  // i think this prevents the user from quitting the app
-  // mainWindow.on('close', function (event) {
-  //   if(!app.isQuiting){
-  //       event.preventDefault();
-  //       mainWindow.hide();
-  //   }
-  //   return false;
-  // });
-
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
-
-
-
-}
-
-
 // WATCHER
 
-function startWatcher(path){
+function startWatcher(path) {
+
   let scanComplete = false;
-    // document.getElementById("start").disabled = true;
-    // document.getElementById("messageLogger").innerHTML = "Scanning the path, please wait ...";
-    watcher = chokidar.watch(path, {
-        ignored: /[\/\\]\./,
-        persistent: true
-    });
-    function onWatcherReady(){
-        console.info('From here can you check for real changes, the initial scan has been completed.');
-        scanComplete = true;
-        // document.getElementById("stop").style.display = "block";
-        // document.getElementById("messageLogger").innerHTML = "The path is now being watched";
-    }
+
+  watcher = chokidar.watch(path, {
+      ignored: /[\/\\]\./,
+      persistent: true
+  });
+
+  function onWatcherReady(){
+      console.info('From here can you check for real changes, the initial scan has been completed.');
+      scanComplete = true;
+  }
+
     watcher
     .on('add', function(path) {
 
       if(scanComplete) {
         if(path.includes("Screen Shot")) {
           console.log("SCREEN SHOT",path);
-          var notif = new Notification({
-            'title': 'New Screenshot',
-            // 'body':  'native html5 notif',
-            'icon': __dirname + '/assets/notificationIcon.png'
-          });
-          notif.show();
+
+          const dialogOptions = {type: 'info', buttons: ['OK', 'Cancel'], message: 'Copy image to clipboard?'}
+
+          dialog.showMessageBox(dialogOptions, i => {
+
+            let screenshotImage = nativeImage.createFromPath(path)
+            console.log(screenshotImage)
+
+            if(i === 0) {
+              clipboard.writeImage(screenshotImage)
+            }
+
+          })
+
+          // var notif = new Notification({
+          //   'title': 'New Screenshot',
+          //   // 'body':  'native html5 notif',
+          //   'icon': __dirname + '/assets/notificationIcon.png'
+          // });
+          // notif.show();
         }
       }
 
     })
-    // .on('addDir', function(path) {
-    //      console.log('Directory', path, 'has been added');
-    //      if(showInLogFlag){
-    //          addLog("Folder added : "+path,"new");
-    //      }
-    //  })
-    // .on('change', function(path) {
-    //     console.log('File', path, 'has been changed');
-    //     if(showInLogFlag){
-    //         addLog("A change ocurred : "+path,"change");
-    //     }
-    // })
-    // .on('unlink', function(path) {
-    //     console.log('File', path, 'has been removed');
-    //     if(showInLogFlag){
-    //         addLog("A file was deleted : "+path,"delete");
-    //     }
-    // })
-    // .on('unlinkDir', function(path) {
-    //     console.log('Directory', path, 'has been removed');
-    //     if(showInLogFlag){
-    //         addLog("A folder was deleted : "+path,"delete");
-    //     }
-    // })
-    // .on('error', function(error) {
-    //     console.log('Error happened', error);
-    //     if(showInLogFlag){
-    //         addLog("An error ocurred: ","delete");
-    //         console.log(error);
-    //     }
-    // })
+
     .on('ready', onWatcherReady)
     .on('raw', function(event, path, details) {
         // This event should be triggered everytime something happens.
