f3da75ee9831f33160b27ff3bc5f971099570983
kevando <khabich@gmail.com>
2018-10-18 21:29:55 -0400

    attempting to run app without an active window

--- src/index.js
+++ src/index.js
@@ -39,22 +39,22 @@ app.on('ready', function() {
 
 
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1000, height: 600});
-
-  // and load the index.html of the app.
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
+  // mainWindow = new BrowserWindow({width: 1000, height: 600});
+  //
+  // // and load the index.html of the app.
+  // mainWindow.loadURL('file://' + __dirname + '/index.html');
+  //
+  // // Open the DevTools.
+  // mainWindow.webContents.openDevTools();
+  //
+  //
+  // // Emitted when the window is closed.
+  // mainWindow.on('closed', function() {
+  //   // Dereference the window object, usually you would store windows
+  //   // in an array if your app supports multi windows, this is the time
+  //   // when you should delete the corresponding element.
+  //   mainWindow = null;
+  // });
 
 
 
